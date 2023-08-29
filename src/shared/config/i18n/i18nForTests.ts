b9e627126ee82362e8c57e223bf82746b399900b
zamukaev <abu.safiia2016@mail.com>
2023-08-29 16:24:20 +0200

    upgrade sidebar add button themes

--- src/shared/config/i18n/i18nForTests.ts
+++ src/shared/config/i18n/i18nForTests.ts
@@ -4,8 +4,7 @@ import { initReactI18next } from 'react-i18next';
 i18n
     .use(initReactI18next)
     .init({
-        lng: 'ru',
-        fallbackLng: 'ru',
+        fallbackLng: 'en',
         debug: false,
         interpolation: {
             escapeValue: false, // not needed for react!!
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,22 +1,22 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import { renderWithTranslation }
-    from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { fireEvent, screen } from '@testing-library/react';
+
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
     test('Sidebar rendering', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
     test('Sidebar have collapsed class', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         fireEvent.click(screen.getByTestId('toggle-btn'));
         expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
         fireEvent.click(screen.getByTestId('toggle-btn'));
         expect(screen.getByTestId('sidebar')).not.toHaveClass('collapsed');
     });
     test('Sidebar have collapsed class', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         fireEvent.click(screen.getByTestId('toggle-btn'));
         expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
         fireEvent.click(screen.getByTestId('toggle-btn'));
